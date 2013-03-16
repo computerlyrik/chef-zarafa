92435338b364e6e5dde98f44e3469b8eddb99c56
shnifti <shnifti@monkeyhood.org>
2013-03-16 23:23:53 +0100

    added timezone for z-push in attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,3 +8,5 @@ default['zarafa']['additional_domains'] = Array.new
 default['zarafa']['backend_type'] = nil
 default['zarafa']['ssl'] = false
 default['zarafa']['vmail_user'] = nil
+
+default['zarafa']['timezone'] = nil
