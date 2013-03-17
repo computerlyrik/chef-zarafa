29dad710dbbbb0db55730c558226f695531b9027
Christian Fischer <sw-dev@computerlyrik.de>
2013-03-17 03:38:19 +0100

    v 1.2.0
    - forced HTTPS-Support for webapp, webaccess and z-push
    - z-push now excluded into recipe
    - timezone-configuration for z-push
    - WiP: virtual mysql-mailboxes
    - WiP: run dagent as vmail user
    - WiP: zarafa mobile device manager recipe

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "chef-cookbooks@computerlyrik.de"
 license          "Apache 2.0"
 description      "Installs/Configures zarafa"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.2.0"
 
 
 %w{ ubuntu debian }.each do |os|
