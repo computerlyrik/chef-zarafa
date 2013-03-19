c0355130a7514e17ca465f74b51e819f2590b36b
Christian Fischer <sw-dev@computerlyrik.de>
2013-03-19 19:07:47 +0100

    fixed dspam recipe for zarafa

--- recipes/dspam.rb
+++ recipes/dspam.rb
@@ -1,8 +1,25 @@
 
 include_recipe "dspam"
 
+
+node.set["clamav"]["clamd"]["tcp_socket"] = 3310
+node.set["clamav"]["clamd"]["tcp_addr"] = "127.0.0.1"
 include_recipe "clamav"
 
 #template "/etc/dspam/dspam.conf"
 
+template "/etc/postfix/dspam_filter_access" do
+  notifies :restart, resources(:service => "postfix")
+end
+
+
+template "/etc/postfix/master.cf" do
+  notifies :restart, resources(:service => "postfix")
+end
+
+
+template "/etc/dspam/dspam.conf" do
+  notifies :restart, resources(:service => "dspam")
+  notifies :restart, resources(:service => "postfix")
+end
 
