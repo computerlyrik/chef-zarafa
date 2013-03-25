67c2c276044cedd31d773104e8afdff308056d00
Christian Fischer <sw-dev@computerlyrik.de>
2013-03-25 23:18:16 +0100

    v1.3.0 - mysql setup complete - bugfixes on postgres

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "chef-cookbooks@computerlyrik.de"
 license          "Apache 2.0"
 description      "Installs/Configures zarafa"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.2.1"
+version          "1.3.0"
 
 
 %w{ ubuntu debian }.each do |os|
