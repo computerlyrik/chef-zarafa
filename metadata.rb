cbe9504df928bfdbb737a99ba60925f2a887d1a8
Christian Fischer <sw-dev@computerlyrik.de>
2013-10-31 22:38:25 +0100

    major release v2 - updated installation process to work out of the box

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "chef-cookbooks@computerlyrik.de"
 license          "Apache 2.0"
 description      "Installs/Configures zarafa"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.3.0"
+version          "2.0.0"
 
 
 %w{ ubuntu debian }.each do |os|
