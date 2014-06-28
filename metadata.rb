e4f4c994345473af4934cb370cd3f586323c5c8f
Christian Fischer <sw-dev@computerlyrik.de>
2014-06-28 04:52:43 +0200

    releasin pre 3.0.0 - working installation on debian with 7.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "chef-cookbooks@computerlyrik.de"
 license          "Apache 2.0"
 description      "Installs/Configures zarafa"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.1.0"
+version          "2.9.0"
 
 
 %w{ ubuntu debian }.each do |os|
