5a68aad9a40ca9d6912f5cd8418457936ec7dd55
Christian Fischer <sw-dev@computerlyrik.de>
2014-08-15 13:28:25 +0200

    ssl recipe

---
+++ recipes/ssl.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: zarafa
+# Recipe:: ssl
+#
+# Copyright 2012, 2014, computerlyrik
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+certificate_manage 'zarafa-gateway' do
+  search_id node['zarafa']['certificate_databag_id']
+  cert_path node['zarafa']['certificate_path']
+  not_if { node['zarafa']['certificate_databag_id'].nil? }
+end
