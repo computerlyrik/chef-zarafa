ff7638315196a86bda7bd0106d50baf9070e495e
Christian Fischer <sw-dev@computerlyrik.de>
2014-08-19 23:47:18 +0200

    release rc-1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "chef-cookbooks@computerlyrik.de"
 license          "Apache 2.0"
 description      "Installs/Configures zarafa"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.9.10"
+version          "2.9.11"
 
 %w(ubuntu debian).each do |os|
   supports os
