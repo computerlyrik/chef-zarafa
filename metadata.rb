740adb1026cbcc6659208576e862a2fab89df74f
Christian Fischer <sw-dev@computerlyrik.de>
2014-08-26 23:43:55 +0200

    pinning mysql version timezone setting is now global smaller fixes
    rc-3

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,13 @@ maintainer_email "chef-cookbooks@computerlyrik.de"
 license          "Apache 2.0"
 description      "Installs/Configures zarafa"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.9.12"
+version          "2.9.13"
 
 %w(ubuntu debian).each do |os|
   supports os
 end
 
-depends 'mysql' '5.3.6'
+depends 'mysql', '= 5.3.6'
 %w(openssl database ark apache2 postfix certificate).each do |dep|
   depends dep
 end
