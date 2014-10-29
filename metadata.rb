2f94340ba275156f34eefb508deaa715af6d8f3a
Christian Fischer <sw-dev@computerlyrik.de>
2014-10-29 20:35:29 +0100

    bumped version to 3.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "chef-cookbooks@computerlyrik.de"
 license          "Apache 2.0"
 description      "Installs/Configures zarafa"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "3.0.1"
+version          "3.1.0"
 
 %w(ubuntu debian).each do |os|
   supports os
